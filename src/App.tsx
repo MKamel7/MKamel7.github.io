2692cf4e7bea5ec1c15aee98574319b61b289627
Mohamed Kamel <mkamel860@gmail.com>
2026-07-05 19:30:27 +0200

    scaffold: vite react-ts + tailwind v4 + fonts

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <main>
+      <section id="about" />
+      <section id="projects" />
+      <section id="experience" />
+      <section id="contact" />
+    </main>
+  )
+}
+
+export default App
